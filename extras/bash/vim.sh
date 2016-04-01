bf59c24edc9fabe64410a0cf9d67801b72f1f79f
Elizabeth Manrique <ecmanrique@gmail.com>
2016-04-01 16:30:39 -0500

    fix(vim): fixed install

--- extras/bash/vim.sh
+++ extras/bash/vim.sh
@@ -10,85 +10,49 @@ url_lua_repo="http://www.lua.org/ftp/$filename_lua.tar.gz"
 uninstall_flag=$1
 
 
-function main {
-    if [[ $uninstall_flag == "--uninstall" ]]; then
-        uninstall_vim
-    else
-        install_vim
-        install_success
-    fi
-}
+echo "-->Go $HOME"
+cd $HOME
 
-function install_vim {
-    echo "-->Go $HOME"
-    cd $HOME
+echo "-->Creating the vim folder..."
+mkdir vim
 
-    echo "-->Creating the vim folder..."
-    mkdir vim
+echo "-->Go to the created vim folder..."
+cd $HOME/vim
 
-    echo "-->Go to the created vim folder..."
-    cd $HOME/vim
+echo "-->Invoque and untar from the mirror hosted in github"
+curl -L $url | tar zx
 
-    echo "-->Invoque and untar from the mirror hosted in github"
-    curl -L $url | tar zx
+echo "-->Go to to $foldername folder"
+cd "$foldername"
 
-    echo "-->Go to to $foldername folder"
-    cd "$foldername"
+echo "-->Cleaning existing build"
+make distclean
 
-    echo "-->Cleaning existing build"
-    make distclean
+echo "-->Executing And build with the default settings"
+./configure --with-features=huge \
+    --enable-multibyte \
+    --enable-rubyinterp \
+    --enable-pythoninterp \
+    --with-python-config-dir=/usr/lib/python2.7/config \
+    --enable-perlinterp \
+    --enable-fail-if-missing \
+    --enable-cscope
 
-    echo "-->Executing And build with the default settings"
-    ./configure --with-features=huge \
-            --enable-multibyte \
-            --enable-rubyinterp \
-            --enable-pythoninterp \
-            --with-python-config-dir=/usr/lib/python2.7/config \
-            --enable-perlinterp \
-            --enable-fail-if-missing \
-            --enable-cscope
-    
-    echo "-->creating the backup of /usr/bin/vim"
-    sudo mv /usr/bin/vim /usr/bin/vim-old
-    
-    echo "-->Execute the installer with superuser mode, "
-    echo "-->basically the binary copy to the path /usr/local/bin"
-    sudo make install
-    
-    
-    echo "-->creating the /usr/local/bin link"
-    sudo ln -s /usr/local/bin/vim /usr/bin/vim
-    
-    echo "-->Exporting the current path"
-    export PATH=/usr/local/bin:$PATH
-    
-    echo "-->Go to $HOME path"
-    cd $HOME
-    echo "-->done!"
-}
+echo "-->creating the backup of /usr/bin/vim"
+sudo mv /usr/bin/vim /usr/bin/vim-old
 
-function uninstall_vim {
-    read -p "Are you sure to uninstall vim? (y/n)" -n 1 -r
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        echo "-->Go to source folder"
-        cd "$HOME/vim/vim-$version"
-        echo "-->uninstalling..."
-        sudo make uninstall
-        
-        echo "-->Welcome back old vim"
-        sudo mv /usr/bin/vim-old /usr/bin/vim
-        
-        cd $HOME
-        sudo rm -fr "$HOME/vim/$foldername"
-        echo "-->done."
-    else
-        exit 1
-    fi
-}
+echo "-->Execute the installer with superuser mode, "
+echo "-->basically the binary copy to the path /usr/local/bin"
+sudo make install
 
-function install_success {
-    echo "-->vim has been successfully installed :D"
-    vim --version
-}
 
-main
+echo "-->creating the /usr/local/bin link"
+sudo ln -s /usr/local/bin/vim /usr/bin/vim
+
+echo "-->Exporting the current path"
+export PATH=/usr/local/bin:$PATH
+
+echo "-->Go to $HOME path"
+cd $HOME
+echo "-->done!"
+
