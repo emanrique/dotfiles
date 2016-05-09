343e947f8ca54f132e6192f754bba9cf63ad53fe
Elizabeth Manrique <ecmanrique@gmail.com>
2016-05-09 17:25:51 -0400

    feat(vim.sh): added vim with custom supports

--- extras/bash/vim.sh
+++ extras/bash/vim.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #Script to install vim 7.4.803 from the vim mirror hosted in github
 
-version="v7.4.803"
-foldername="vim-7.4.803"
+version="v7-4-803"
+foldername="vim-7-4-803"
 url="https://github.com/vim/vim/archive/$version.tar.gz"
 
 filename_lua="lua-5.1"
@@ -10,49 +10,85 @@ url_lua_repo="http://www.lua.org/ftp/$filename_lua.tar.gz"
 uninstall_flag=$1
 
 
-echo "-->Go $HOME"
-cd $HOME
+function main {
+    if [[ $uninstall_flag == "--uninstall" ]]; then
+        uninstall_vim
+    else
+        install_vim
+        install_success
+    fi
+}
 
-echo "-->Creating the vim folder..."
-mkdir vim
+function install_vim {
+    echo "-->Go $HOME"
+    cd $HOME
 
-echo "-->Go to the created vim folder..."
-cd $HOME/vim
+    echo "-->Creating the vim folder..."
+    mkdir vim
 
-echo "-->Invoque and untar from the mirror hosted in github"
-curl -L $url | tar zx
+    echo "-->Go to the created vim folder..."
+    cd $HOME/vim
 
-echo "-->Go to to $foldername folder"
-cd "$foldername"
+    echo "-->Invoque and untar from the mirror hosted in github"
+    curl -L $url | tar zx
 
-echo "-->Cleaning existing build"
-make distclean
+    echo "-->Go to to $foldername folder"
+    cd "$foldername"
 
-echo "-->Executing And build with the default settings"
-./configure --with-features=huge \
-    --enable-multibyte \
-    --enable-rubyinterp \
-    --enable-pythoninterp \
-    --with-python-config-dir=/usr/lib/python2.7/config \
-    --enable-perlinterp \
-    --enable-fail-if-missing \
-    --enable-cscope
+    echo "-->Cleaning existing build"
+    make distclean
 
-echo "-->creating the backup of /usr/bin/vim"
-sudo mv /usr/bin/vim /usr/bin/vim-old
+    echo "-->Executing And build with the default settings"
+    ./configure --with-features=huge \
+            --enable-multibyte \
+            --enable-rubyinterp \
+            --enable-pythoninterp \
+            --with-python-config-dir=/usr/lib/python2.7/config \
+            --enable-perlinterp \
+            --enable-fail-if-missing \
+            --enable-cscope
 
-echo "-->Execute the installer with superuser mode, "
-echo "-->basically the binary copy to the path /usr/local/bin"
-sudo make install
+    echo "-->creating the backup of /usr/bin/vim"
+    sudo mv /usr/bin/vim /usr/bin/vim-old
+
+    echo "-->Execute the installer with superuser mode, "
+    echo "-->basically the binary copy to the path /usr/local/bin"
+    sudo make install
 
 
-echo "-->creating the /usr/local/bin link"
-sudo ln -s /usr/local/bin/vim /usr/bin/vim
+    echo "-->creating the /usr/local/bin link"
+    sudo ln -s /usr/local/bin/vim /usr/bin/vim
 
-echo "-->Exporting the current path"
-export PATH=/usr/local/bin:$PATH
+    echo "-->Exporting the current path"
+    export PATH=/usr/local/bin:$PATH
 
-echo "-->Go to $HOME path"
-cd $HOME
-echo "-->done!"
+    echo "-->Go to $HOME path"
+    cd $HOME
+    echo "-->done!"
+}
 
+function uninstall_vim {
+    read -p "Are you sure to uninstall vim? (y/n)" -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]; then
+        echo "-->Go to source folder"
+        cd "$HOME/vim/vim-$version"
+        echo "-->uninstalling..."
+        sudo make uninstall
+
+        echo "-->Welcome back old vim"
+        sudo mv /usr/bin/vim-old /usr/bin/vim
+
+        cd $HOME
+        sudo rm -fr "$HOME/vim/$foldername"
+        echo "-->done."
+    else
+        exit 1
+    fi
+}
+
+function install_success {
+    echo "-->vim has been successfully installed :D"
+    vim --version
+}
+
+main
